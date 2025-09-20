c820a1f560f2959fe190c92e30183d969e39f67d
nateschuckers <nateschuckers@gmail.com>
2025-09-20 03:44:09 -0400

    Add files via upload

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import ProfileMenu from './ProfileMenu';
 
-const Header = ({ user, ...props }) => (
+const Header = ({ user, onLogout, onSwitchView, theme, toggleTheme, currentView, onShowTour }) => (
     <header className="bg-neutral-100 dark:bg-neutral-800 p-4 flex justify-between items-center border-b border-neutral-200 dark:border-neutral-700">
         <div>
             <img src="/assets/logo-light.png" alt="Company Logo" className="h-8 dark:hidden" />
@@ -10,14 +10,28 @@ const Header = ({ user, ...props }) => (
         </div>
         {user && (
             <div className="flex items-center space-x-4">
-                <ProfileMenu user={user} {...props} />
+                <ProfileMenu 
+                    user={user} 
+                    onLogout={onLogout} 
+                    onSwitchView={onSwitchView} 
+                    theme={theme} 
+                    toggleTheme={toggleTheme} 
+                    currentView={currentView} 
+                    onShowTour={onShowTour} 
+                />
             </div>
         )}
     </header>
 );
 
 Header.propTypes = {
-    user: PropTypes.object.isRequired,
+    user: PropTypes.object,
+    onLogout: PropTypes.func,
+    onSwitchView: PropTypes.func,
+    theme: PropTypes.string,
+    toggleTheme: PropTypes.func,
+    currentView: PropTypes.string,
+    onShowTour: PropTypes.func,
 };
 
 export default Header;
